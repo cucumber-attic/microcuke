8871c9c81a08b1a4d71750b8e611fb9c1829bba4
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-10-21 14:42:54 +0100

    First commit, flesh out a rough Runtime API

---
+++ test/cucumber/runtime_test.js
@@ -0,0 +1,34 @@
+var assert = require('assert');
+var Runtime = require('../../lib/cucumber/runtime');
+
+describe("Runtime", function () {
+  describe("#execute", function () {
+    it("returns 1 when there is a failing test case", function () {
+      var failedTestCase = {
+        execute: function(eventEmitter) {
+          var failedStep = {
+            result: { status: 'failed' }
+          }
+          eventEmitter.emit('step-finished', failedStep);
+        }
+      }
+      var runtime = new Runtime([failedTestCase]);
+      var exitStatus = runtime.execute();
+      assert.equal(exitStatus, 1);
+    });
+
+    it("returns 0 when all test cases are passing", function () {
+      var passedTestCase = {
+        execute: function(eventEmitter) {
+          var passedStep = {
+            result: { status: 'passed' }
+          }
+          eventEmitter.emit('step-finished', passedStep);
+        }
+      }
+      var runtime = new Runtime([passedTestCase]);
+      var exitStatus = runtime.execute();
+      assert.equal(exitStatus, 0);
+    });
+  });
+});
