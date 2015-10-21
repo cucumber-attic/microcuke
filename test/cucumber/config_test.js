6530813e676dbd1c47895b5fce331b373b527619
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-10-21 16:27:21 +0100

    Extract stubs

--- test/cucumber/config_test.js
+++ test/cucumber/config_test.js
@@ -2,42 +2,47 @@ var EventEmitter = require('events').EventEmitter;
 var assert = require('assert');
 var Config = require('../../lib/cucumber/config');
 
+var stubGlueLoader = {
+  loadGlue: function () {
+    return {
+      createTestSteps: function () {
+        return [
+          {
+            execute: function (eventEmitter) {
+              eventEmitter.emit('step-started');
+            }
+          }
+        ];
+      }
+    };
+  }
+};
+
+function stubGherkinLoader(gherkin) {
+  return {
+    loadGherkinFiles: function () {
+      return [{
+        path: 'features/hello.feature',
+        read: function () {
+          return gherkin;
+        }
+      }];
+    }
+  };
+}
+
 describe("Config", function () {
   describe("#build", function () {
     it("builds executable TestCase object from Gherkin sources and Glue code", function () {
       var config = new Config();
 
-      var glueLoader = {
-        loadGlue: function () {
-          return {
-            createTestSteps: function () {
-              return [
-                {
-                  execute: function (eventEmitter) {
-                    eventEmitter.emit('step-started');
-                  }
-                }
-              ];
-            }
-          };
-        }
-      };
+      var gherkinLoader = stubGherkinLoader("" +
+        "Feature: Hello\n" +
+        "  Scenario: first\n" +
+        "    Then this should pass\n"
+      );
 
-      var gherkinLoader = {
-        loadGherkinFiles: function () {
-          return [{
-            path: 'features/hello.feature',
-            read: function () {
-              return "" +
-                "Feature: Hello\n" +
-                "  Scenario: first\n" +
-                "    Then this should pass"
-            }
-          }];
-        }
-      };
-
-      var testCases = config.buildTestCases(glueLoader, gherkinLoader);
+      var testCases = config.buildTestCases(stubGlueLoader, gherkinLoader);
       var eventEmitter = new EventEmitter();
 
       var stepExecuted = false;
@@ -53,40 +58,16 @@ describe("Config", function () {
     it("builds 2 TestCase objects from Gherkin sources and Glue code", function () {
       var config = new Config();
 
-      var glueLoader = {
-        loadGlue: function () {
-          return {
-            createTestSteps: function () {
-              return [
-                {
-                  execute: function (eventEmitter) {
-                    eventEmitter.emit('step-started');
-                  }
-                }
-              ];
-            }
-          };
-        }
-      };
+      var gherkinLoader = stubGherkinLoader("" +
+        "Feature: Hello\n" +
+        "  Scenario: first\n" +
+        "    Then this should pass\n" +
+        "\n" +
+        "  Scenario: second\n" +
+        "    Then this should pass\n"
+      );
 
-      var gherkinLoader = {
-        loadGherkinFiles: function () {
-          return [{
-            path: 'features/hello.feature',
-            read: function () {
-              return "" +
-                "Feature: Hello\n" +
-                "  Scenario: first\n" +
-                "    Then this should pass\n" +
-                "\n" +
-                "  Scenario: second\n" +
-                "    Then this should pass\n"
-            }
-          }];
-        }
-      };
-
-      var testCases = config.buildTestCases(glueLoader, gherkinLoader);
+      var testCases = config.buildTestCases(stubGlueLoader, gherkinLoader);
       assert.equal(testCases.length, 2);
     });
   });
