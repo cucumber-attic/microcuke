abfcb832bdf8f39aa720ca056da2ddaa5d630b43
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-10-21 15:29:54 +0100

    Consistently named tests

--- test/cucumber/config_test.js
+++ test/cucumber/config_test.js
@@ -2,18 +2,20 @@ var assert = require('assert');
 var Config = require('../../lib/cucumber/config');
 
 describe("Config", function () {
-  it("builds TestCase objects from Gherkin sources and Glue code", function () {
-    var config = new Config();
+  describe("#build", function () {
+    it("builds TestCase objects from Gherkin sources and Glue code", function () {
+      var config = new Config();
 
-    var glueLoader = function() {
+      var glueLoader = function () {
 
-    };
+      };
 
-    var gherkinLoader = function() {
+      var gherkinLoader = function () {
 
-    };
+      };
 
-    var testCases = config.buildTestCases(glueLoader, gherkinLoader);
-    assert.equal(testCases.length, 1);
-  })
+      var testCases = config.buildTestCases(glueLoader, gherkinLoader);
+      assert.equal(testCases.length, 1);
+    });
+  });
 });
