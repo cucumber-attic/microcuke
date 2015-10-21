3cb3d26659a30a842567e462644ae155fe7c2c51
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-10-21 15:45:37 +0100

    Use Gherkin compiler to build pickles, then TestCases

--- test/cucumber/config_test.js
+++ test/cucumber/config_test.js
@@ -3,19 +3,53 @@ var Config = require('../../lib/cucumber/config');
 
 describe("Config", function () {
   describe("#build", function () {
-    it("builds TestCase objects from Gherkin sources and Glue code", function () {
+    it("builds 1 TestCase object from Gherkin sources and Glue code", function () {
       var config = new Config();
 
-      var glueLoader = function () {
+      var loadGlue = function () {
 
       };
 
-      var gherkinLoader = function () {
-
+      var loadGherkinFiles = function () {
+        return [{
+          path: 'features/hello.feature',
+          read: function() {
+            return "" +
+              "Feature: Hello\n" +
+              "  Scenario: first\n" +
+              "    Then this should pass"
+          }
+        }];
       };
 
-      var testCases = config.buildTestCases(glueLoader, gherkinLoader);
+      var testCases = config.buildTestCases(loadGlue, loadGherkinFiles);
       assert.equal(testCases.length, 1);
+    });
+
+    it("builds 2 TestCase objects from Gherkin sources and Glue code", function () {
+      var config = new Config();
+
+      var loadGlue = function () {
+
+      };
+
+      var loadGherkinFiles = function () {
+        return [{
+          path: 'features/hello.feature',
+          read: function() {
+            return "" +
+              "Feature: Hello\n" +
+              "  Scenario: first\n" +
+              "    Then this should pass\n" +
+              "\n" +
+              "  Scenario: second\n" +
+              "    Then this should pass\n"
+          }
+        }];
+      };
+
+      var testCases = config.buildTestCases(loadGlue, loadGherkinFiles);
+      assert.equal(testCases.length, 2);
     });
   });
 });
