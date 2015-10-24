4e75d329c0c0d49c51629c544def0e1525fa6734
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-10-24 22:24:59 +0100

    Added the start of a CLI for manual testing

---
+++ bin/microcuke-cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+var Glue = require('../lib/cucumber/glue');
+var GlueLoader = require('../lib/cucumber/glue_loader');
+var PickleLoader = require('../lib/cucumber/pickle_loader');
+var Runtime = require('../lib/cucumber/runtime');
+
+var glueLoader = new GlueLoader();
+var pickleLoader = new PickleLoader();
+
+var glue = glueLoader.loadGlue("test-data", Glue);
+var pickles = pickleLoader.loadPickles("test-data");
+var testCases = pickles.map(glue.createTestCase);
+var runtime = new Runtime(testCases);
+process.exit(runtime.execute());
+
