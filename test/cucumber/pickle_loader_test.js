d350bb3b97a8ca8e5b708eaf3e9f335be07d688f
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-10-25 21:04:06 +0000

    Load individual files

--- test/cucumber/pickle_loader_test.js
+++ test/cucumber/pickle_loader_test.js
@@ -3,9 +3,16 @@ var PickleLoader = require('../../lib/cucumber/pickle_loader');
 
 describe("PickleLoader", function () {
   describe("#loadPickles", function () {
-    it("builds executable TestCase object from Gherkin sources and Glue code", function () {
+    it("loads pickles from directory", function () {
       var pickleLoader = new PickleLoader();
       var pickles = pickleLoader.loadPickles('test-data');
+      assert.equal(pickles.length, 2);
+      assert.equal(pickles[0].steps[0].text, "first step");
+    });
+
+    it("loads pickles from file", function () {
+      var pickleLoader = new PickleLoader();
+      var pickles = pickleLoader.loadPickles('test-data/hello.feature');
       assert.equal(pickles.length, 2);
       assert.equal(pickles[0].steps[0].text, "first step");
     });
