9a806b80173f4052c6d539c59d23ee78fead1c9c
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-10-26 11:39:02 +0000

    Prep the ground for tag filters

--- test/cucumber/pickle_loader_test.js
+++ test/cucumber/pickle_loader_test.js
@@ -1,25 +1,40 @@
 var assert = require('assert');
 var PickleLoader = require('../../lib/cucumber/pickle_loader');
 
+function passthrough(pickle) {
+  return true;
+}
+
 describe("PickleLoader", function () {
   describe("#loadPickles", function () {
     it("loads pickles from directory", function () {
-      var pickleLoader = new PickleLoader();
+      var pickleLoader = new PickleLoader(passthrough);
       var pickles = pickleLoader.loadPickles('test-data');
       assert.equal(pickles.length, 2);
       assert.equal(pickles[0].steps[0].text, "first step");
     });
 
     it("loads pickles from file", function () {
-      var pickleLoader = new PickleLoader();
+      var pickleLoader = new PickleLoader(passthrough);
       var pickles = pickleLoader.loadPickles('test-data/hello.feature');
       assert.equal(pickles.length, 2);
       assert.equal(pickles[0].steps[0].text, "first step");
     });
 
     it("filters pickles by line number", function () {
-      var pickleLoader = new PickleLoader();
-      var pickles = pickleLoader.loadPickles('test-data/hello.feature:5:55');
+      var pickleLoader = new PickleLoader(passthrough);
+      var pickles = pickleLoader.loadPickles('test-data/hello.feature:99:5');
+      assert.equal(pickles.length, 1);
+      assert.equal(pickles[0].steps[0].text, "second step");
+    });
+
+    it("accepts additional filters", function () {
+      function otherFilter(pickle) {
+        return pickle.steps[0].text == 'second step';
+      }
+
+      var pickleLoader = new PickleLoader(otherFilter);
+      var pickles = pickleLoader.loadPickles('test-data/hello.feature');
       assert.equal(pickles.length, 1);
       assert.equal(pickles[0].steps[0].text, "second step");
     });
